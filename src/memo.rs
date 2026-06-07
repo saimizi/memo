4907734e9db82161c637f4bdcf4a814d82e9e9cb
Seimizu Joukan <joukan.seimizu@gmail.com>
2026-06-07 10:24:44 +0900

    style: use imported env::var() instead of fully qualified path

--- src/memo.rs
+++ src/memo.rs
@@ -341,7 +341,7 @@ pub struct Memo {
 #[allow(unused)]
 impl Memo {
     fn setup_root(root_path: Option<&str>) -> Result<(String, String), MemoError> {
-        let home = std::env::var("HOME").unwrap_or_else(|_| ".".to_string());
+        let home = env::var("HOME").unwrap_or_else(|_| ".".to_string());
         let mut root = format!("{home}/.memo");
 
         if let Some(r) = root_path {
